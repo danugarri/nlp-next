f8a594055c4677a5cbf6e708d2f65c271b404baa
danugarri <danugarri@hotmail.com>
2024-03-16 23:42:45 +0100

    feature: Performance improvement

--- src/app/components/Spinner/Spinner.tsx
+++ src/app/components/Spinner/Spinner.tsx
@@ -4,7 +4,7 @@ import { CircularProgress } from '@mui/material';
 const Spinner = () => {
   return (
     <CircularProgress
-      sx={{ color: '#000', position: 'absolute', top: '50vh', left: '50vw', transform: 'translate(-50%, -50%)' }}
+      sx={{ color: '#000', position: 'absolute', top: '50vh', left: '50vw' }}
     />
   );
 };
