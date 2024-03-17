2c9676cdd4378eefbf8b70f05c01ce26ed4a6ab9
danugarri <danugarri@hotmail.com>
2024-03-17 12:32:57 +0100

    chore: updated next config following npm llamaindex

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
+export const runtime = 'nodejs'; // default
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['pdf2json'],
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      sharp$: false,
+      'onnxruntime-node$': false,
+    };
+    return config;
+  },
+};
 
 export default nextConfig;
