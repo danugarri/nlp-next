337096aa834599fc28eeb61b82f3134ea3975244
danugarri <danugarri@hotmail.com>
2024-03-19 19:18:50 +0100

    Fix: Adjustment for production

--- src/utils/fileReader.ts
+++ src/utils/fileReader.ts
@@ -1,10 +1,12 @@
+import path from 'path';
 import fs from 'fs/promises';
 import { fileName, mainRoute } from '../../public/consts';
 import { FileReaderError } from '@/app/services/errors';
 
 export default async function fileReader() {
   try {
-    const content = await fs.readFile(`${mainRoute}${fileName}`, 'utf-8');
+    const filePath = path.join(process.cwd(), mainRoute, fileName);
+    const content = await fs.readFile(filePath, 'utf-8');
     return content;
   } catch (error) {
     throw new FileReaderError('Error when reading the react.md file');
