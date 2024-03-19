8692d8ad53a84e77ed2c98d7d898dae614ab5315
danugarri <danugarri@hotmail.com>
2024-03-19 19:45:46 +0100

    Fix: Added env variable for the md file path

--- src/utils/fileReader.ts
+++ src/utils/fileReader.ts
@@ -1,10 +1,10 @@
 import fs from 'fs/promises';
-import { fileName, mainRoute } from '../../public/consts';
 import { FileReaderError } from '@/app/services/errors';
 
 export default async function fileReader() {
   try {
-    const content = await fs.readFile(`${mainRoute}${fileName}`, 'utf-8');
+    const filePath = process.env.MDFILE_PATH!;
+    const content = await fs.readFile(filePath, 'utf-8');
     return content;
   } catch (error) {
     throw new FileReaderError('Error when reading the react.md file');
